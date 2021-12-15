2ac82ff25b46ceeab022716beab607d239ea650e
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-12-15 16:10:17 +0300

    library function

---
+++ lib.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int my_strlen(char* s);
+int my_strcmp(char* s1, char* s2);
+char *my_strchr(char *s, int c);
+char* my_strcpy(char* s1, char* s2);
+char* my_strdup (char *src);
+char * my_strstr(char * string1, char * string2 );
+char * my_memchr(char * str, char val, size_t num); 
+char * my_memset(char * str, char val, size_t num);
+char *my_memcpy (char * src, int n);
