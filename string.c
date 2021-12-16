17ad045fd637dfe8cd3a67f405957a782e5da325
error404cyber <67262189+error404cyber@users.noreply.github.com>
2021-12-16 09:40:03 +0300

    all strings

--- string.c
+++ string.c
@@ -34,12 +34,13 @@ int my_strcmp(char* s1, char* s2) {
     return 0;}
   if (s2 == NULL){
     return 0;}
-  int i = 0;
-  while (s1[i] != 0) {
-    if (s1[i] != s2[i]) {
-	return s1[i] - s2[i];}
-    i=i+1;}
-  return s1[i] - s2[i];
+  while (*s1) {
+    if (*s1 != *s2) {
+	return *s1 - *s2;}
+    *s1++;
+    *s2++;  
+  }
+  return *s1 - *s2;
 }
 
 /**
